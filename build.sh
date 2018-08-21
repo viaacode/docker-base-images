27f8a91eeaa4c98fb0e5f36df48e20758a405213
Herwig Bogaert <herwig.bogaert@viaa.be>
2018-08-21 14:26:03 +0200

    use stretch-slim

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-docker pull docker.io/debian:jessie
-docker build -t debian:jessie debian
-docker tag -f debian:jessie debian:latest
+docker pull docker.io/debian:stretch-slim
+docker build -t debian:stretch-slim debian
+docker tag debian:stretch-slim debian:latest
+docker tag debian:stretch-slim debian:latest
 
 docker pull docker.io/centos:7
 docker build -t centos:7 centos
-docker tag -f centos:7 centos:latest
+docker tag centos:7 centos:latest
 
 DANGLING=$(docker images -q -f dangling=true)
 [ -n "$DANGLING" ] && docker rmi $DANGLING
